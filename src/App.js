43413da65b8b6d80be071eab4a3e64c6b07d4171
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-02-24 14:02:31 +0330

    added footer link to repo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
       <header className="App-header">
         <h1>Weather React App</h1>
       </header>
+      <footer>
+        this project was coded by{" "}
+        <a
+          href="https://cool-starship-bcbb97.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ro?a
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Rojahm/react-weather-app-v4"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
