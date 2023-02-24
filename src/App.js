4da8f0972b4aceac456b42c732bff43ccd41922a
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-02-24 18:43:23 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather React App</h1>
-        <button className="btn btn-success">bootstrap</button>
-      </header>
-      <footer>
-        this project was coded by{" "}
-        <a
-          href="https://cool-starship-bcbb97.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-          className="App-link"
-        >
-          Ro?a
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Rojahm/react-weather-app-v4"
-          target="_blank"
-          rel="noreferrer"
-          className="App-link"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather React App</h1>
+        </header>
+        <form>
+          <input type="submit" className="btn btn-primary" value="submit" />
+        </form>
+        <button className="btn btn-primary">bootstrap</button>
+        <footer>
+          this project was coded by{" "}
+          <a
+            href="https://cool-starship-bcbb97.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="App-link"
+          >
+            Ro?a
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Rojahm/react-weather-app-v4"
+            target="_blank"
+            rel="noreferrer"
+            className="App-link"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
