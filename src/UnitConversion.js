6845319a565730fc23f316e8bd52730446635fd0
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-03-04 23:50:48 +0330

    removed unit conversion

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -3,55 +3,19 @@ import "./UnitConversion.css";
 
 function UnitConversion(props) {
   let celsius = Math.round(props.metric);
-  let fahrenheit = Math.round((props.metric * 9) / 5 + 32);
-  const [unit, setUnit] = useState("metric");
 
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("metric");
-  }
-
-  if (unit === "metric") {
-    return (
-      <div className="UnitConversion">
-        <div className="temp">
-          <h2 className="px-2">{celsius}</h2>
-        </div>
-
-        <div className="unit">
-          <span>°C</span>
-          <span>|</span>
-          <span>
-            <a href="/" onClick={showFahrenheit}>
-              °F
-            </a>
-          </span>
-        </div>
+  return (
+    <div className="UnitConversion">
+      <div className="temp">
+        <h2 className="px-2">{celsius}</h2>
       </div>
-    );
-  } else {
-    return (
-      <div className="UnitConversion">
-        <div className="temp">
-          <h2 className="px-2">{fahrenheit}</h2>
-        </div>
 
-        <div className="unit">
-          <span>
-            <a href="/" onClick={showCelsius}>
-              °C
-            </a>
-          </span>
-          <span>|</span>
-          <span>°F</span>
-        </div>
+      <div className="unit">
+        <span>°C</span>
+        <span>|</span>
+        <span>°F</span>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default UnitConversion;
